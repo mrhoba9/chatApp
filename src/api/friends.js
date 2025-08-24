e9dff0598648d944510d9d9bcba8ef649c587d67
mrhoba9 <amadoehab@gmail.com>
2025-08-24 14:24:10 +0300

    Created friends api request front-end

---
+++ src/api/friends.js
@@ -0,0 +1,43 @@
+import axios from "axios";
+
+const API = axios.create({
+    baseURL: "http://localhost:3000/api",
+    withCredentials: true,
+})
+
+// send request
+export async function outgoingRequest(receiverPublicKey){
+    const res = await API.post("/friends/request", { receiverPublicKey });
+    return res.data;
+}
+
+// get request
+export async function incomingRequests(){
+    const res = await API.get("/friends/receive");
+    return res.data;
+}
+
+// accept request
+export async function approveRequest(userApprovedPublicKey){
+    const res = await API.post("/friends/approve", { userApprovedPublicKey });
+    return res.data;
+}
+
+// reject request
+export async function rejectRequest(userRejectedPublicKey){
+    const res = await API.post("/friends/reject", { userRejectedPublicKey });
+    return res.data;
+}
+
+// reject request
+export async function listFriends(){
+    const res = await API.get("/friends/list");
+    return res.data;
+}
+
+// remove friend
+export async function removeFriend(friendPublicKey){
+    const res = await API.post("/friends/unfriend", { friendPublicKey });
+    return res.data;
+}
+
